e040c927f14d666bcf5a22d2ddcddd17a3b41c4a
Lampert Milan <lamp.milan@gmail.com>
2026-08-10 11:17:38 +0200

    Version label moved on top of the scene

--- version_label.gd
+++ version_label.gd
@@ -2,12 +2,4 @@ extends Label
 
 
 func _ready() -> void:
-	text = _build_ver_str()
-
-
-func _build_ver_str() -> String:
-	var major: int = Version.MAJOR
-	var minor: int = Version.MINOR
-	var patch: int = Version.PATCH
-	
-	return "%d.%d.%d" % [major, minor, patch]
+	text = Version.build_ver_str()
